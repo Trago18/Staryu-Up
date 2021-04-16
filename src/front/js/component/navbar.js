b21f4127f0546953367ac24da71cb5ef93c7f207
Lvargasp77 <vargaspaniagua77@gmail.com>
2021-04-16 22:49:43 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,77 +5,75 @@ import "../../styles/navbar.scss";
 export const Navbar = () => {
 	return (
 		<>
-			{/* <!--Buscador--> */}
-			<nav className="navbar navbar-inverse">
-				<div className="container height d-flex align-items-center flex-column">
-					<form className="navbar-form navbar-left" action="/action_page.php">
-						<div className="input-group">
-							<input
-								className="form-control mr-sm-2 rounded-pill px-lg-5"
-								type="search"
-								placeholder="Buscador..."
-								aria-label="Search"
-							/>
-							<button className="btn btn-info my-2 my-sm-0" type="submit">
-								BUSCAR
-							</button>
-							<div className="input-group-btn">
-								<button className="btn btn-default" type="submit">
-									<i className="glyphicon glyphicon-search" />
-								</button>
-							</div>
+			{/* <!-- Navbar--> */}
+			<div className="container-fluid">
+				<div className="row navbar">
+					<nav className="nav navbar navbar-expand-lg navbar-light fixed-top">
+						<a className="navbar-brand text-white" href="#">
+							<h1 className="display-3">A</h1>
+						</a>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-toggle="collapse"
+							data-target="#navbarNav"
+							aria-controls="navbarNav"
+							aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon" />
+						</button>
+						<div className="collapse navbar-collapse flex-column" id="navbarNav">
+							<ul className="navbar-nav">
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										<h6>
+											<bold>INICIO</bold>
+										</h6>
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										<h6>
+											<bold>CATEGORÍA</bold>
+										</h6>
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										<h6>
+											<bold>PERFÍL</bold>
+										</h6>
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										<h6>
+											<bold>CONTÁCTENOS</bold>
+										</h6>
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link text-white" href="#">
+										<h6>
+											<bold>INICIAR SECIÓN</bold>
+										</h6>
+									</a>
+								</li>
+							</ul>
+							{/* <!--Buscador--> */}
+							<form className="form-inline">
+								<div className="input-group">
+									<input
+										type="text"
+										className="form-control rounded-pill border-dark"
+										placeholder="Buscador"
+									/>
+								</div>
+							</form>
 						</div>
-					</form>
+					</nav>
 				</div>
-			</nav>
-			{/* <!-- Navigation--> */}
-			<nav className="navbar navbar-expand-lg fixed-top" id="mainNav">
-				<div className="container">
-					<a className="navbar-brand js-scroll-trigger px-lg-1" href="#page-top">
-						APP
-					</a>
-					<button
-						className="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarResponsive"
-						aria-controls="navbarResponsive"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						Menu
-						<i className="fas fa-bars" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarResponsive">
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 rounded js-scroll-trigger" href="#portfolio">
-									INICIO
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#about">
-									CATEGORIAS
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#contact">
-									PERFIL
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#contact">
-									CONTCTENOS
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-5 rounded js-scroll-trigger" href="#contact">
-									INICIAR SECIÓN
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+			</div>
 		</>
 	);
 };
