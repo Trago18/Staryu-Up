69900d71f814965df6e078c725e3d2e0b0f46a08
Lvargasp77 <vargaspaniagua77@gmail.com>
2021-04-19 02:35:51 +0000

    footer con efecto

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,87 +2,9 @@ import React from "react";
 import "../../styles/home.scss";
 import polygonoImageUrl from "../../img/polygono.png";
 
-/* <!--import cakeImageUrl from "../../img/cake.png";
-import circusImageUrl from "../../img/circus.png";
-import gameImageUrl from "../../img/game.png";
-import safeImageUrl from "../../img/safe.png";
-import submarineImageUrl from "../../img/submarine.png";
-import polygonoImageUrl from "../../img/polygono.png";-->*/
-
 export const Home = () => {
 	return (
 		<body id="page-top">
-			{/* <!--Navbar-->
-			<nav className="navbar navbar-inverse">
-				<div className="container height d-flex align-items-center flex-column">
-					<form className="navbar-form navbar-left" action="/action_page.php">
-						<div className="input-group">
-							<input
-								className="form-control mr-sm-2 rounded-pill px-lg-5"
-								type="search"
-								placeholder="Buscador..."
-								aria-label="Search"
-							/>
-							<button className="btn btn-info my-2 my-sm-0" type="submit">
-								BUSCAR
-							</button>
-							<div className="input-group-btn">
-								<button className="btn btn-default" type="submit">
-									<i className="glyphicon glyphicon-search" />
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</nav>
-			<nav className="navbar navbar-expand-lg fixed-top" id="mainNav">
-				<div className="container">
-					<a className="navbar-brand js-scroll-trigger px-lg-1" href="#page-top">
-						APP
-					</a>
-					<button
-						className="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarResponsive"
-						aria-controls="navbarResponsive"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						Menu
-						<i className="fas fa-bars" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarResponsive">
-						<ul className="navbar-nav ml-auto">
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 rounded js-scroll-trigger" href="#portfolio">
-									INICIO
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#about">
-									CATEGORIAS
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#contact">
-									PERFIL
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-2 rounded js-scroll-trigger" href="#contact">
-									CONTCTENOS
-								</a>
-							</li>
-							<li className="nav-item mx-0 mx-lg-1">
-								<a className="nav-link py-3 px-0 px-lg-5 rounded js-scroll-trigger" href="#contact">
-									INICIAR SECIÓN
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav> */}
-
 			{/* <!-- Masthead--> */}
 			<header className="masthead bg-white text-center">
 				<div className="container p-5 height d-flex align-items-center flex-column">
@@ -163,30 +85,7 @@ export const Home = () => {
 					<h2 className="page-section-heading pt-5">UNASE A NUESTRA RED DE EXPERTOS</h2>
 				</div>
 			</section>
-			{/* <!-- Nosotrso--> */}
-			<section className="page-section-us mb-0">
-				<div className="container ">
-					<h2 className="page-section-us pt-5">SOBRE NOSOTROS</h2>
-				</div>
-			</section>
 
-			{/* <!-- Copyright Section--> *
-			<div className="copyright py-4 text-center text-white">
-				<div className="container">
-					<small>
-						Copyright &copy; Your Website
-						{/* <!-- This script automatically adds the current year to your website footer--> */}
-			{/* <!-- (credit: https://updateyourfooter.com/)--> 
-						<script>document.write(new Date().getFullYear());</script>
-					</small>
-				</div>
-			</div>
-			<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)--> 
-			<div className="scroll-to-top d-lg-none position-fixed">
-				<a className="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
-                        <i className="fa fa-chevron-up" />
-				</a>
-			</div>--*/}
 			{/* ########################################################################################################################################################## */}
 			{/* <!-- Portfolio Modals--> */}
 			{/* <!-- Portfolio Modal 1--> */}
