d21448ce2c3187bf5ba500a309bf80e60a9426e1
Trago18 <franksolorc@hotmail.com>
2021-04-19 20:50:29 +0000

    data test

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,7 +7,6 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
-
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
@@ -16,6 +15,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/data-test', methods=['GET'])
+def data_test():
+    user=User(first_name='nombre1', last_name='apellido1', phone_number = 'numero1', email = 'test1@gmail.com', password = 'Pass123*', is_active = True)
+    db.session.add(user)
+    user=User(first_name='nombre2', last_name='apellido2', phone_number = 'numero2', email = 'test2@gmail.com', password = 'Pass234*', is_active = True)
+    db.session.add(user)
+
+    suppiler=Supplier(name='proveedor1', phone_number='numero1', email='test1@gmail.com', category='random', profile_pic='url', address='cr', description='test', rate=5, member_since='2021', user_id=1)
+    db.session.add(suppiler)
+    suppiler=Supplier(name='proveedor2', phone_number='numero2', email='test2@gmail.com', category='random', profile_pic='url', address='cr', description='test', rate=5, member_since='2021', user_id=1)
+    db.session.add(suppiler)
+    suppiler=Supplier(name='proveedor3', phone_number='numero3', email='test3@gmail.com', category='random', profile_pic='url', address='cr', description='test', rate=5, member_since='2021', user_id=2)
+    db.session.add(suppiler)
+
+    db.session.commit()
+    return jsonify("ok"), 200
+
 
 @api.route('/', methods=['POST'])
 def search():
