704298f0231a8fe9c26dfa15ffe68a7e59005653
Trago18 <franksolorc@hotmail.com>
2021-04-21 00:08:03 +0000

    email

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,6 +7,9 @@ from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, current_user, jwt_required, JWTManager
 from datetime import timedelta
 import re
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
 
 
 api = Blueprint('api', __name__)
@@ -37,6 +40,22 @@ def data_test():
     db.session.commit()
     return jsonify("ok"), 200
 
+
+@api.route('/password-recovery', methods=['POST'])   # recuperar contraseña
+def get_password():
+
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("franksolorc@hotmail.com")
+    to_email = To("franksolorc@gmail.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+    return jsonify({'msg': 'The email was send.'})
 
 @api.route('/', methods=['POST'])   # Barra de busqueda
 def search():
