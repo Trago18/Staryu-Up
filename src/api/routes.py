22363d3d428913619ed0e358bcb7152a85cdb2e4
Trago18 <franksolorc@hotmail.com>
2021-04-21 04:15:04 +0000

    password recovery

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import create_access_token, current_user, jwt_required, 
 from datetime import timedelta
 import re
 import os
-import sendgrid
+from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import *
 
 
@@ -44,11 +44,11 @@ def data_test():
 @api.route('/password-recovery', methods=['POST'])   # recuperar contraseña
 def get_password():
 
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email("franksolorc@hotmail.com")
-    to_email = To("franksolorc@gmail.com")
-    subject = "Sending with SendGrid is Fun"
-    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("franksolorc@hotmail.com", 'Trago')
+    to_email = To("majorooc09@gmail.com")
+    subject = "I'm the BEST"
+    content = Content("text/plain", "Ya está funcionando lo de mandar correos xD")
     mail = Mail(from_email, to_email, subject, content)
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
