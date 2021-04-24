f30ee8ba20425b06b392477976bf3a20b57321d4
tebanban <estebancalf@gmail.com>
2021-04-24 17:41:23 +0000

    user profile updated

--- src/front/js/pages/userProfile.js
+++ src/front/js/pages/userProfile.js
@@ -3,12 +3,15 @@ import "../../styles/userProfile.scss";
 import { Media, Tab, Tabs, Container, Row, Col, Button } from "react-bootstrap";
 import imgGarden from "../../img/landscapingvert.jpg";
 import googleMaps from "../../img/googleMaps.jpg";
+import { Context } from "../store/appContext";
 
 export const User_Profile = () => {
+	const { store } = useContext(Context);
+
 	return (
 		<Container className="bg-light my-5">
 			<Row>
-				<div className="userCard p-4 " style={{ width: "100%", height: "200px" }}>
+				<div className="userCard p-4 " style={{ width: "100%", height: "140px" }}>
 					<Media>
 						<img
 							width={120}
@@ -23,46 +26,61 @@ export const User_Profile = () => {
 							</div>
 							<span>Poás, Alajuela</span>
 						</Media.Body>
-						<Button href="#">Ver mi perfil de Experto</Button>
+						<Button className="userButton m-2" href="#">
+							Ver mi perfil de Experto
+						</Button>
 					</Media>
 				</div>
 			</Row>
 			<Row>
-				<Tabs defaultActiveKey="cuenta" id="uncontrolled-tab-example" className="tabs">
-					<Tab eventKey="cuenta" title="Mi Cuenta">
+				<Tabs defaultActiveKey="cuenta" id="uncontrolled-tab-example" className="tabs pr-5 justify-content-end">
+					<Tab eventKey="cuenta" title="Mi Cuenta" className="userTab">
 						<div className="userAccount pl-3 pt-3">
-							<p>
-								<b>Usuario:</b>
-							</p>
-							<p>
-								<b>Email:</b>
-							</p>
-							<p>
-								<b>Contraseña:</b>
-							</p>
+							<div>
+								<span>Email: {store.userData.email}</span>
+								<hr />
+							</div>
+							<div>
+								<span>
+									Contraseña:
+									{store.userData.password}
+								</span>
+								<hr />
+							</div>
 						</div>
-						<hr />
+
 						<div className="userInfo pl-3 pb-3">
-							<p>
-								<b>Nombre y apellido:</b>
-							</p>
-							<p>
-								<b>Email:</b>
-							</p>
-							<p>
-								<b>Teléfono:</b>
-							</p>
+							<div>
+								<span>
+									Nombre y apellido: {store.userData.first_name + " " + store.userData.last_name}
+								</span>
+							</div>
+							<hr />
+							<div>
+								<span>
+									Teléfono:
+									{store.userData.phone_number}
+								</span>
+								<hr />
+							</div>
 						</div>
 					</Tab>
-					<Tab eventKey="favoritos" title="Favoritos">
-						<>
-							When in disgrace with fortune and men's eyes I all alone beweep my outcast state, And
-							trouble deaf heaven with my bootless cries, And look upon myself, and curse my fate, Wishing
-							me like to one more rich in hope, Featur'd like him,{" "}
-						</>
-					</Tab>
-					<Tab eventKey="transacciones" title="Transacciones" disabled>
-						<> </>
+					<Tab eventKey="favoritos" title="Favoritos" className="favoritesTab">
+						<div className="userAccount pl-3 pt-3">
+							<div>
+								<h5>Mi lista de favoritos:</h5>
+								<hr />
+								<ul>
+									<li />
+									<hr />
+									<li />
+									<hr />
+									<li />
+									<hr />
+									<li />
+								</ul>
+							</div>
+						</div>
 					</Tab>
 				</Tabs>
 			</Row>
