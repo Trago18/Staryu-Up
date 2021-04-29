b4076374e22d8609dd69a1c8a9abe7580ba5a526
Trago18 <franksolorc@hotmail.com>
2021-04-29 19:21:31 +0000

    stars

--- src/api/routes.py
+++ src/api/routes.py
@@ -120,9 +120,9 @@ def supplier_rate(id):
     rates = Rates.query.filter_by(supplier_id=id).all()
     all_rates = list(map(lambda x: x.serialize(), rates))
     # print(sum(all_rates)/len(all_rates))
-    
+
     supplier = Supplier.query.filter_by(id=id).first()
-    supplier.rate = sum(all_rates)/len(all_rates)
+    supplier.rate = (sum(all_rates)+5)/(len(all_rates)+1)
     db.session.commit()
     
     return jsonify(supplier.rate), 200
